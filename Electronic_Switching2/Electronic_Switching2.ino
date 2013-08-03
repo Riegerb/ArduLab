e1c5783ad667c04b3ca06a4d4b925adbf0263674
Brian Rieger <rieger.brian12@gmail.com>
2013-08-02 21:19:48 -0700

    initial code dump

---
+++ Electronic_Switching2/Electronic_Switching2.ino
@@ -0,0 +1,33 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+ 
+  This example code is in the public domain.
+ */
+ 
+// Pin 13 has an LED connected on most Arduino boards.
+// give it a name:
+
+
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+
+  
+  DDRH |= B00000011;
+  DDRJ |= B00001000;
+}
+
+// the loop routine runs over and over again forever:
+void loop() 
+{
+  
+  PORTH |= B00000011;
+  PORTJ |= B00001000;
+  delay(100000);
+  
+  PORTH &= !(B00000011);
+  PORTJ &= !(B00001000);
+  delay(100000);
+  
+}
---
+++ Electronic_Switching_4/Electronic_Switching_4.ino
@@ -0,0 +1,44 @@
+int experimentTimeSeconds = 30;
+int dataModeTimeSeconds = 30;
+int ADG = 17;
+int TS = 16;
+int flag = 0;
+unsigned long time;
+unsigned long currenttime;
+
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  Serial.begin(9600);
+  time = millis();
+  Serial.print(time);
+  pinMode(ADG, OUTPUT);
+  pinMode(TS, OUTPUT); 
+   digitalWrite(TS, HIGH); 
+  digitalWrite(ADG, HIGH); 
+  
+  
+  
+  
+  
+  
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  
+   currenttime = millis();  
+  if (currenttime-time<dataModeTimeSeconds*1000){
+  flag = 1;
+  digitalWrite(TS, LOW);
+  digitalWrite(ADG, LOW);
+
+  }
+  
+ if (currenttime-time>experimentTimeSeconds*1000){
+  digitalWrite(TS, HIGH); 
+  digitalWrite(ADG, HIGH);
+  flag=2;
+  }
+  
+}
