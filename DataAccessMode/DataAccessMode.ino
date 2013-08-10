2745c1be9590065566a8e8cf8c16f09f6eb42c20
Brian Rieger <rieger.brian12@gmail.com>
2013-08-09 19:57:13 -0700

    added instructions to DataAccessMode.ino

--- DataAccessMode/DataAccessMode.ino
+++ DataAccessMode/DataAccessMode.ino
@@ -8,10 +8,43 @@ new drive will appear in 'My Computer'.
 Make sure you have an SD Card plugged into
 the ArduLab before uploading this program.
 
-NOTE: This program switches the ArduLab to
-Data Access Mode Indefinitely, a jumper
-must be removed to get back to Experiment
-Mode and/or to upload a new program.
+How this works:
+Once this code is uploaded, the ArduLab will
+remain in Experiment Mode for 30 seconds,
+it will then switch to Data Access Mode and
+a new drive will appear on your computer.
+The ArduLab will stay in Data Access Mode for
+30 seconds during which time you can access
+everything on the SD card as you would any
+USB Thumb Drive. After 30 seconds, the
+ArduLab will switch back to Experiment Mode.
+
+YOU CAN ONLY UPLOAD A PROGRAM TO THE ARDULAB
+WHEN IT IS IN EXPERIMENT MODE.
+
+When the ArduLab is in Data Access Mode, the
+Blue LED next to the 'Data Acces' label on
+the backside of the ArduLab will be
+illuminated. Therefore, when the blue LED is
+not illuminated, you can upload programs to
+the ArduLab.
+
+If you would like to change the time period
+each mode is active, increase or decrease the
+number inside the delay(##) function. The
+number inside is in Milliseconds therefore,
+every 1000 is equivalent to one second. To
+change the time it stays in Experiment Mode,
+modify the number inside the first delay(##)
+function. If you would like to change how
+long it stays in Data Access mode, change
+the second delay(##) function.
+
+For example, if you wanted the ArduLab to
+stay in Experiment Mode for 10 seconds and
+in Data Access for 1 minute, you would
+modify the first delay(##) to 'delay(10000);'
+and the second delay to 'delay(60000);'
 
 */
 
